16f7ebbaba93df1cff621bc425b343ab0fcb400a
acrisanto <alanvcrisanto@gmail.com>
2022-08-17 23:28:18 -0500

    applied transition to button

--- advice-generator-app-main/js/main.js
+++ advice-generator-app-main/js/main.js
@@ -3,7 +3,6 @@ const URL = "https://api.adviceslip.com/advice";
 const getAdvices = async () => {
 	const response = await fetch(URL);
 	const data = await response.json();
-	console.log(data);
 
 	let adviceID = document.querySelector(".advice-id");
 	let adviceText = document.querySelector(".advice-text");
